a474d2fc21f896a372494d13c8aca5a9db691af3
Natalie Perpepaj <perpepajn25@gmail.com>
2017-09-24 15:20:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 // Add your doToElementsInArray() function here:
+var evens = [0, 2, 4, 6, 8, 10];
 
+evens.forEach(even => {
+  console.log(`${even} is not odd!`);
+});
 
 // Add your changeCompletely() function here:
+function changeCompletely(element, index, array) {
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+}
+
+function doToElementsInArray(array, callback) {
+  array.forEach(callback);
+}
+
+var animals = ["dog", "cat", "squirrel"];
+doToElementsInArray(animals, changeCompletely);
+
+console.log(animals);
